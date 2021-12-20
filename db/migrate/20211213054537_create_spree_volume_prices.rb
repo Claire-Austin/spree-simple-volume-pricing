6ec0cd70cc255f008438d7cd664b90c878769221
vish-bluebash <bluebash@bluebash.co>
2021-12-20 13:25:33 +0530

    migration fixes.

--- db/migrate/20211213054537_create_spree_volume_prices.rb
+++ db/migrate/20211213054537_create_spree_volume_prices.rb
@@ -2,8 +2,8 @@ class CreateSpreeVolumePrices < ActiveRecord::Migration[6.0]
   def change
     create_table :spree_volume_prices do |t|
       t.references :variant
-      t.integer :starting_quantity
-      t.decimal :price
+      t.integer :starting_quantity, null: false
+      t.decimal :price, precision: 8, scale: 2, null: false, default: 0.0
 
       t.timestamps
     end
