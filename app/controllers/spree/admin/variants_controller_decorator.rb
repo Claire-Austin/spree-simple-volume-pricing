21856ad64836c4d841d518472987705e4be4fdeb
bluebash-dev <developer@bluebash.co>
2022-01-21 15:24:56 +0530

    minor bug fix.

--- app/controllers/spree/admin/variants_controller_decorator.rb
+++ app/controllers/spree/admin/variants_controller_decorator.rb
@@ -1,14 +1,13 @@
 module Spree::Admin::VariantsControllerDecorator
-
   def self.prepended(base)
-    base.after_action :create_volume_prices, only: [:create, :update]
+    base.after_action :create_volume_prices, only: %i[create update]
   end
 
   def create_volume_prices
     return unless params[:variant][:volume_prices_attributes].present?
 
     params[:variant][:volume_prices_attributes].values.each do |volume|
-      next if volume[:price].blank?
+      next if volume[:price].to_f.zero? || volume[:price].blank?
 
       volume_price = @variant.volume_prices.find_by(id: volume[:id])
       if volume_price.present?
@@ -24,7 +23,7 @@ module Spree::Admin::VariantsControllerDecorator
 
   def volume_prices_attributes_params(attributes)
     {
-      starting_quantity: attributes[:starting_quantity], 
+      starting_quantity: attributes[:starting_quantity],
       price: attributes[:price]
     }
   end
