c816c4195291f276ab8af7dabed10013640f2bb1
bluebash-dev <developer@bluebash.co>
2022-02-24 17:08:17 +0530

    UI changes.

--- app/overrides/spree/products/cart_form.rb
+++
@@ -1,8 +0,0 @@
-# insert_before :order_details_subtotal, :partial => "shared/order_details_volume_discount"
-
-Deface::Override.new(
-  virtual_path: 'spree/products/_cart_form',
-  name: 'Add store ids hidden field in form',
-  insert_before: 'div[class="add-to-cart-form-delivery"]',
-  partial: 'spree/products/volume_prices'
-)
