4ac771d5e8f645da1c1f1e560a5542cdc045e4c1
bluebash-dev <developer@bluebash.co>
2022-05-06 11:23:32 +0530

    minor bug fix.

--- app/overrides/spree/admin/prepend_order_details_vd_override.rb
+++ app/overrides/spree/admin/prepend_order_details_vd_override.rb
@@ -3,6 +3,6 @@
 Deface::Override.new(
   virtual_path: 'spree/checkout/_summary',
   name: 'prepend_order_details_subtotal',
-  insert_before: "erb[silent]:contains('if order.adjustments.nonzero.eligible.exists?')",
+  insert_before: "erb[silent]:contains('if order.passed_checkout_step?')",
   partial: 'spree/shared/order_details_volume_discount'
 )
